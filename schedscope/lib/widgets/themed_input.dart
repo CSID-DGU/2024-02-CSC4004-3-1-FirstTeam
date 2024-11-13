d6c4498843a9342d850ee0f2b35e32b77c7c14f5
kohs2k21 <gw010101@naver.com>
2024-11-13 20:23:55 +0900

    feat: 스타일이 적용된 text input 위젯 개발

---
+++ schedscope/lib/widgets/themed_input.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ThemedInput extends StatelessWidget {
+  final String labelText;
+  final bool obscureText;
+  final TextEditingController controller;
+  final Widget? suffixIcon;
+
+  const ThemedInput({
+    super.key,
+    required this.labelText,
+    this.obscureText = false,
+    required this.controller,
+    this.suffixIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: const TextStyle(color: Color(0xFF999999)),
+        filled: true,
+        fillColor: Colors.grey[100],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: BorderSide.none,
+        ),
+        suffixIcon: suffixIcon,
+      ),
+    );
+  }
+}
