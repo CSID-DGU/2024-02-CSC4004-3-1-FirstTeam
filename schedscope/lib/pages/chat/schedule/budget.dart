a59b89dc7d0658646fc8e374b247322bdeb23f18
kohs2k21 <gw010101@naver.com>
2024-12-02 22:32:51 +0900

    feat: 예산 수정 기능 추가

--- schedscope/lib/pages/chat/schedule/budget.dart
+++ schedscope/lib/pages/chat/schedule/budget.dart
@@ -20,9 +20,9 @@ class IndividualBudgetPage extends StatelessWidget {
     final budgetItems = budgetState.schedules[scheduleId] ?? [];
     final NumberFormat formatter = NumberFormat('#,###');
 
-    // 합계 계산
-    int totalAmount =
-        budgetItems.fold<int>(0, (sum, item) => sum + (item['amount'] as int));
+    // // 합계 계산
+    // int totalAmount =
+    //     budgetItems.fold<int>(0, (sum, item) => sum + (item['amount'] as int));
 
     return Scaffold(
       appBar: AppBar(
@@ -70,95 +70,197 @@ class IndividualBudgetPage extends StatelessWidget {
                 children: [
                   const Text(
                     '합계',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
                   ),
                   const SizedBox(width: 10),
                   Text(
-                    '${formatter.format(budgetState.getTotalAmount(scheduleId))}원',
+                    formatter.format(budgetState.getTotalAmount(scheduleId)),
                     style: const TextStyle(
                       fontSize: 28,
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
                     ),
                   ),
+                  const Text(" 원",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ))
                 ],
               ),
             ),
           ),
+          // 항목 리스트
           // 항목 리스트
           Expanded(
             child: ListView.builder(
               itemCount: budgetItems.length,
               itemBuilder: (context, index) {
                 final item = budgetItems[index];
-                return Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.grey,
-                          blurRadius: 5,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 12, horizontal: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          item['name'],
-                          style: const TextStyle(fontSize: 18),
-                        ),
-                        Text(
-                          '${formatter.format(item['amount'])}원',
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+                return GestureDetector(
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        String updatedName = item['name'];
+                        int updatedAmount = item['amount'];
+
+                        return AlertDialog(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
                           ),
-                        ),
-                        const SizedBox(width: 8),
-                        // 삭제 아이콘
-                        IconButton(
-                          icon: const Icon(Icons.delete, color: Colors.black),
-                          onPressed: () async {
-                            final bool? confirmDelete = await showDialog<bool>(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: const Text('삭제 확인'),
-                                  content: const Text('정말로 해당 항목을 삭제하시겠습니까?'),
-                                  actions: <Widget>[
-                                    TextButton(
+                          title: const Text(
+                            '비용 수정',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18,
+                            ),
+                          ),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextField(
+                                decoration:
+                                    const InputDecoration(labelText: '항목 이름'),
+                                controller:
+                                    TextEditingController(text: item['name']),
+                                onChanged: (value) {
+                                  updatedName = value;
+                                },
+                              ),
+                              TextField(
+                                decoration:
+                                    const InputDecoration(labelText: '금액'),
+                                keyboardType: TextInputType.number,
+                                controller: TextEditingController(
+                                    text: item['amount'].toString()),
+                                onChanged: (value) {
+                                  updatedAmount = int.tryParse(value) ?? 0;
+                                },
+                              ),
+                            ],
+                          ),
+                          actions: [
+                            SizedBox(
+                              width: double.infinity, // 다이얼로그의 가로 크기 채우기
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly, // 균등하게 배치
+                                children: [
+                                  Expanded(
+                                    child: TextButton(
+                                      style: TextButton.styleFrom(
+                                        side: const BorderSide(
+                                            color: Colors.blue),
+                                      ),
                                       onPressed: () {
-                                        Navigator.of(context).pop(false); // 취소
+                                        Navigator.pop(context); // 다이얼로그 닫기
                                       },
                                       child: const Text('취소'),
                                     ),
-                                    TextButton(
+                                  ),
+                                  const SizedBox(width: 10), // 버튼 간 간격 조정
+                                  Expanded(
+                                    child: ElevatedButton(
                                       onPressed: () {
-                                        Navigator.of(context).pop(true); // 확인
+                                        if (updatedName.isNotEmpty &&
+                                            updatedAmount > 0) {
+                                          // 항목 업데이트 로직 추가
+                                          budgetState.updateBudgetItem(
+                                              roomId,
+                                              scheduleId,
+                                              index,
+                                              updatedName,
+                                              updatedAmount);
+                                          Navigator.pop(context);
+                                        }
                                       },
-                                      child: const Text('삭제'),
+                                      child: const Text('수정'),
                                     ),
-                                  ],
-                                );
-                              },
-                            );
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 5,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            item['name'],
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                          Text(
+                            '${formatter.format(item['amount'])}원',
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          // 삭제 아이콘
+                          IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.black),
+                            onPressed: () async {
+                              final bool? confirmDelete =
+                                  await showDialog<bool>(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('삭제 확인'),
+                                    content: const Text('정말로 해당 항목을 삭제하시겠습니까?'),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context)
+                                              .pop(false); // 취소
+                                        },
+                                        child: const Text('취소'),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop(true); // 확인
+                                        },
+                                        child: const Text('삭제'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
 
-                            // 사용자가 확인 버튼을 눌렀을 때만 삭제
-                            if (confirmDelete == true) {
-                              budgetState.removeBudgetItem(
-                                  roomId, scheduleId, index);
-                            }
-                          },
-                        ),
-                      ],
+                              // 사용자가 확인 버튼을 눌렀을 때만 삭제
+                              if (confirmDelete == true) {
+                                budgetState.removeBudgetItem(
+                                    roomId, scheduleId, index);
+                              }
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
@@ -207,22 +309,42 @@ class IndividualBudgetPage extends StatelessWidget {
                         ],
                       ),
                       actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text('취소'),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            if (newName.isNotEmpty && newAmount > 0) {
-                              budgetState.addBudgetItem(roomId, scheduleId,
-                                  {'name': newName, 'amount': newAmount});
-                              Navigator.pop(context);
-                            }
-                          },
-                          child: const Text('추가'),
-                        ),
+                        SizedBox(
+                          width: double.infinity, // 다이얼로그의 가로 크기 채우기
+                          child: Row(
+                            mainAxisAlignment:
+                                MainAxisAlignment.spaceEvenly, // 균등하게 배치
+                            children: [
+                              Expanded(
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                    side: const BorderSide(color: Colors.blue),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.pop(context); // 다이얼로그 닫기
+                                  },
+                                  child: const Text('취소'),
+                                ),
+                              ),
+                              const SizedBox(width: 10), // 버튼 간 간격 조정
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    if (newName.isNotEmpty && newAmount > 0) {
+                                      budgetState.addBudgetItem(
+                                          roomId, scheduleId, {
+                                        'name': newName,
+                                        'amount': newAmount
+                                      });
+                                      Navigator.pop(context);
+                                    }
+                                  },
+                                  child: const Text('추가'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
                       ],
                     );
                   },
